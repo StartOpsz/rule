faedfb5d1d4a0575a2adbc07ee328c0b938f5d06
cherzx@sina.com <qx@startops.com.cn>
2022-01-19 10:52:00 +0800

    1

--- pkg/errCode/errCode.go
+++ pkg/errCode/errCode.go
@@ -1,12 +1,17 @@
 package errCode
 
 const (
-	Normal = 0
-	NotFound = 404
-	ParameterNotFound = 1404
-	DatabaseOpError = 1201
+	NormalCode = 0
+	NormalMsg = "ok"
+	
+	NotFoundCode = 14040
+	NotFoundMsg = "资源未发现"
+	
+	ParameterNotFoundCode = 14041
+	ParameterNotFoundMsg = "参数缺失"
+	
+	DatabaseOpErrorCode = 17001
+	DatabaseOpErrorMsg = "内部系统异常，请核对code。"
+	
+	
 )
-
-func TT() {
-
-}
