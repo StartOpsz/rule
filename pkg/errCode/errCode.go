a901f492212bae3b543c3c4a17cd0909deb06132
cherzx@sina.com <qx@startops.com.cn>
2022-01-23 23:09:59 +0800

    first push

--- pkg/errCode/errCode.go
+++ pkg/errCode/errCode.go
@@ -2,22 +2,26 @@ package errCode
 
 const (
 	NormalCode = 0
-	NormalMsg = "ok"
-	
+	NormalMsg  = "ok"
+
 	NotFoundCode = 14040
-	NotFoundMsg = "资源未发现"
-	
+	NotFoundMsg  = "资源未发现"
+
 	UserPermissionDenyCode = 14030
-	UserPermissionDenyMsg = "用户权限拒绝"
-	
+	UserPermissionDenyMsg  = "用户权限拒绝"
+
 	ParameterNotFoundCode = 14041
-	ParameterNotFoundMsg = "参数缺失"
-	
+	ParameterNotFoundMsg  = "参数缺失"
+
 	DatabaseOpErrorCode = 17001
-	DatabaseOpErrorMsg = "内部系统异常，请核对code。"
-	
+	DatabaseOpErrorMsg  = "内部系统异常，请核对code。"
+
 	BizOpErrorCode = 17002
-	BizOpErrorMsg = "业务代码异常"
-	
-	
+	BizOpErrorMsg  = "业务代码异常"
+
+	LimitRequestCode = 18001
+	LimitRequestMsg  = "请求频率限制"
+
+	CircuitBreakerCode = 18002
+	CircuitBreakerMsg  = "熔断限制"
 )
