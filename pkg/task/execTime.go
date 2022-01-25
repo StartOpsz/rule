a1bde7fe33b501130fc2221bc41334b5bef8abca
cherzx@sina.com <qx@startops.com.cn>
2022-01-25 16:45:12 +0800

    1

---
+++ pkg/task/execTime.go
@@ -0,0 +1,19 @@
+package task
+
+import "time"
+
+type ExecTime struct {
+	StartTime string
+	EndTime   string
+	CostTime  int64
+}
+
+func ExecTimeResult(startTime time.Time) ExecTime {
+	endTime := time.Now()
+	return ExecTime{
+		StartTime: startTime.String(),
+		EndTime: endTime.String(),
+		CostTime: endTime.Sub(startTime).Milliseconds(),
+	}
+}
+
