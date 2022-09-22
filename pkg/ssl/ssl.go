be62fcd06c1a3a4c2d9036441ef7d6fb691ee696
qx <qx@startops.com.cn>
2022-09-22 10:43:29 +0800

    fix ssl certificate parse

--- pkg/ssl/ssl.go
+++ pkg/ssl/ssl.go
@@ -3,6 +3,8 @@ package ssl
 import (
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
+	"fmt"
 	"time"
 )
 
@@ -17,24 +19,37 @@ type CertificateInfo struct {
 }
 
 func sslCertificateObj(sslCertificateContent []byte) (*x509.Certificate, error) {
-
+	
 	sslCertificatePemDecode, _ := pem.Decode(sslCertificateContent)
-
+	
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("except: ", r)
+		}
+	}()
+	
 	certificateObj, err := x509.ParseCertificate(sslCertificatePemDecode.Bytes)
-
+	
 	return certificateObj, err
 }
 
-// 解析SSL证书信息
+/*
+解析SSL证书信息
+*/
+
 func ParseSSLCertificate(sslCertificateContent []byte) (c *CertificateInfo, err error) {
 	var certificateInfo CertificateInfo
-
+	
 	sslCertificateX509Obj, err := sslCertificateObj(sslCertificateContent)
-
+	
 	if err != nil {
 		return nil, err
 	}
-
+	
+	if sslCertificateX509Obj == nil {
+		return nil, errors.New("格式异常")
+	}
+	
 	certificateInfo.Version = sslCertificateX509Obj.Version
 	certificateInfo.NotBefore = sslCertificateX509Obj.NotBefore //.Format("2006-01-02 15:04")
 	certificateInfo.NotAfter = sslCertificateX509Obj.NotAfter   //.Format("2006-01-02 15:04")
@@ -42,17 +57,20 @@ func ParseSSLCertificate(sslCertificateContent []byte) (c *CertificateInfo, err 
 	certificateInfo.Subject = sslCertificateX509Obj.Subject.String()
 	certificateInfo.SerialNumber = sslCertificateX509Obj.SerialNumber.String()
 	certificateInfo.DNSNames = sslCertificateX509Obj.DNSNames
-
+	
 	return &certificateInfo, nil
 }
 
-// 校验域名是否匹配SSL证书
+/*
+校验域名是否匹配SSL证书
+*/
+
 func VerifyHostName(sslCertificateContent []byte, domain string) error {
 	sslCertificateX509Obj, err := sslCertificateObj(sslCertificateContent)
-
+	
 	if err != nil {
 		return err
 	}
-
+	
 	return sslCertificateX509Obj.VerifyHostname(domain)
 }
--- pkg/ssl/ssl_test.go
+++ pkg/ssl/ssl_test.go
@@ -1,9 +1,15 @@
 package ssl
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestParseSSLCertificate(t *testing.T) {
-
+	_, err := ParseSSLCertificate([]byte("asdsd"))
+	
+	assert.Nil(t, err)
+	
 }
 
 func TestVerifyHostName(t *testing.T) {
